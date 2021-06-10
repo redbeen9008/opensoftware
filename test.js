b66e050af8eb14997fac62fca3b766ac22688b8f
yujin-8808 <obsession8@naver.com>
2021-06-10 22:19:03 +0900

    삭제함수 추가

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 console.log("3대 운동 기록 측정 프로그램");
-var listarr= new crt2DArr(4,4);
-
+var listarr= new crt2DArr(4,5);
+listarr[1]=["1-1",0,0,0,0];
+delete_data(listarr,"1-1");
+console.log(listarr);
 
 
 function crt2DArr(rows, columns) {
@@ -9,4 +11,12 @@ function crt2DArr(rows, columns) {
         arr[i] = new Array(columns);
     }
     return arr;
+}
+
+function delete_data(arr,date_val){
+    for(var i =0;i<4;i++){
+        if(arr[i][0]==date_val){
+            arr[i]=new Array(5);
+        }
+    }
 }
