5aa6f885404e77836ea777327e4497215124255e
redbeen9008 <woo987620@naver.com>
2021-06-10 18:41:32 +0900

    초기파일커밋

---
+++ test.js
@@ -0,0 +1,12 @@
+console.log("3대 운동 기록 측정 프로그램");
+
+
+
+
+function crt2DArr(rows, columns) {
+    var arr = new Array(rows);
+    for (var i = 0; i < rows; i++) {
+        arr[i] = new Array(columns);
+    }
+    return arr;
+}
